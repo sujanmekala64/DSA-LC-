6db46db6d4ba4abc8ec1b0a5fa567739dfbeff42
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-02-08 08:24:41 +0530

    Time: 9 ms (95.58%) | Memory: 56.7 MB (60.96%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode middlenode(ListNode head){
+        ListNode slowptr = head;
+        ListNode fastptr = head.next;
+        while(fastptr!=null && fastptr.next!=null){
+            slowptr = slowptr.next;
+            fastptr = fastptr.next.next;
+        }
+        return slowptr;
+    }
+    public ListNode sorted(ListNode list1,ListNode list2){
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val>list2.val){
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
+            else{
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            }
+        }
+        if(list1!=null) temp.next = list1;
+        if(list2!=null) temp.next = list2;
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head; 
+        ListNode middle = middlenode(head);
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
+        left = sortList(left);
+        right = sortList(right);
+        return sorted(left,right);
+    }
+}
