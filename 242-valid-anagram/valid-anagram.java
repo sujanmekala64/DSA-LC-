cb23bed7161355426445ad44e04163d5449b51d5
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-02-08 23:11:58 +0530

    Time: 1 ms (99.84%) | Memory: 43.3 MB (74.18%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int count[] = new int[26];
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            count[c-'a']--;
+        }
+        for(int co:count){
+            if(co!=0) return false;
+        }
+        return true;
+    }
+}
