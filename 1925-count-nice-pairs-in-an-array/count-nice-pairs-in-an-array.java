12885c97d78631ad5b1abcd7d57038d07372f17d
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-02-09 09:44:39 +0530

    Time: 930 ms (5.06%) | Memory: 60 MB (29.96%) - LeetSync

---
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countNicePairs(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int nicepairs=0;
+        int MOD=1_000_000_007;
+        for(int i=0;i<nums.length;i++){
+            int val = nums[i];
+            StringBuilder sb = new StringBuilder(String.valueOf(val));
+            int reverse = Integer.parseInt(sb.reverse().toString());
+            int tot = val-reverse;
+            System.out.println(val+" "+reverse+" "+tot);
+            if(!map.containsKey(tot)) map.put(tot,1);
+            else{
+                nicepairs+=map.get(tot);
+                nicepairs=nicepairs%MOD;
+                map.put(tot,map.get(tot)+1);
+            }
+        }
+        return nicepairs;
+    }
+}
