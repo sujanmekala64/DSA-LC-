299c5ca6c9da480fafbea3e9b8d59bcc840f2a9e
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-02-11 11:34:18 +0530

    Time: 1 ms (99.46%) | Memory: 43.3 MB (46.53%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,8 +1,26 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        while(s.contains(part)){
-            s=s.replaceFirst(part,"");
+        // while(s.contains(part)){
+        //     s=s.replaceFirst(part,"");
+        // }
+        // return s;
+        int idx = s.indexOf(part);
+        while(idx!=-1){
+            s=s.substring(0,idx)+s.substring(idx+part.length());
+            idx=s.indexOf(part);
         }
         return s;
     }
 }
+
+// class Solution {
+// public:
+//     string removeOccurrences(string s, string part) {
+//         int i = s.find(part);
+//         while(i!=-1){
+//             s = s.substr(0,i) + s.substr(i+part.size());
+//             i = s.find(part);
+//         }
+//         return s;
+//     }
+// };
