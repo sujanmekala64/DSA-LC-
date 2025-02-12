f727e2c055f839d262d1816b5a325aa38af69fba
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-02-12 16:49:22 +0530

    Time: 3 ms (71.15%) | Memory: 40.4 MB (57.66%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num==1)
+            return false;
+        int c=0,f=0;
+        int x=(int)Math.sqrt(num);
+        for(int i=2;i<=x;i++){
+            if(num%i==0){
+                c+=i+(num/i);
+            }
+        }
+        System.out.println(c);
+        return c==num-1?true:false;
+    }
+}
