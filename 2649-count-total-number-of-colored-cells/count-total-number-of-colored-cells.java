874821b1e0c594fd50a1a5852396b054bd2d615b
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-03-05 11:59:11 +0530

    Time: 3 ms (42.21%) | Memory: 40.5 MB (59.02%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -8,5 +8,16 @@ class Solution {
             n--;
         }
         return count;
+
+        /* u can also do this by enumeration 
+            since f(1)=1, f(2)=5, f(3)=13
+            it shows some pattern f(1)=1, f(2)=f(1)+4, f(3)=f(2)+8
+            so the pattern is f(n)=f(n-1)+((n-1)*4)
+            so it will be 1+4+8+12+....(n-1)
+            1+4*(1+2+3+..(n-1))
+            1+4*((n-1)*n)/2
+            1+2*((n-1)*n)
+            so it can also be 1+(long)(2*n*(n-1))
+        */
     }
 }
