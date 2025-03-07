256da3b1e366162d3b1b9758185c0913b9a065c9
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-03-07 08:33:37 +0530

    Time: 102 ms (48.80%) | Memory: 50.2 MB (43.60%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -1,32 +1,30 @@
 class Solution {
-    public boolean isPrime(int n){
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
     public int[] closestPrimes(int left, int right) {
-        List<Integer> li = new ArrayList<>();
-        if(left==1) left++;
-        for(int i=left;i<=right;i++){  
-            if(i==2 || i%2!=0){
-                if(isPrime(i)) li.add(i);
+        boolean prime[] = new boolean[right+1];
+        Arrays.fill(prime,true);
+        for(int i=2;i<=Math.sqrt(right);i++){
+            if(prime[i]==true){
+                for(int j=i*i;j<=right;j+=i){
+                    prime[j]=false;
+                }
             }
         }
-        if(li.size()<=1) return new int[]{-1,-1};
-        int ans[] = new int[2];
+        List<Integer> li = new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            if(i!=1 && prime[i]==true) li.add(i);
+        }
         int mini=Integer.MAX_VALUE;
         int first=-1;
-        int last=-1;
+        int second=-1;
+        if(li.size()<=1) return new int[]{-1,-1};
         for(int i=1;i<li.size();i++){
-            if(li.get(i)-li.get(i-1)<mini){
+            int val = li.get(i)-li.get(i-1);
+            if(val<mini){
                 first=li.get(i-1);
-                last=li.get(i);
-                mini=li.get(i)-li.get(i-1);
+                second=li.get(i);
+                mini=val;
             }
         }
-        ans[0]=first;
-        ans[1]=last;
-        return ans;
+        return new int[]{first,second};
     }
 }
