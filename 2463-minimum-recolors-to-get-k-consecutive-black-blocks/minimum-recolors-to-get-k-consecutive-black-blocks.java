60eb7f9296eddacfb5cb0a434f58bb8903e63c5b
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-03-09 08:06:40 +0530

    Time: 1 ms (92.09%) | Memory: 41.5 MB (58.39%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,19 +1,27 @@
 class Solution {
-    public int check(int start,int end,String blocks){
-        int cnt=0;
-        for(int i=start;i<end;i++){
-            if(blocks.charAt(i)=='W') cnt++;
-        }
-        return cnt;
-    }
+    // public int check(int start,int end,String blocks){
+    //     int cnt=0;
+    //     for(int i=start;i<end;i++){
+    //         if(blocks.charAt(i)=='W') cnt++;
+    //     }
+    //     return cnt;
+    // }
     public int minimumRecolors(String blocks, int k) {
-        int idx=0;
+        int blacks=0;
         int mini=Integer.MAX_VALUE;
-        while(idx+k<=blocks.length()){
-            int whites = check(idx,idx+k,blocks);
-            mini=Math.min(mini,whites);
-            idx++;
+        for(int i=0;i<blocks.length();i++){
+            if(i-k>=0 && blocks.charAt(i-k)=='B') blacks--;
+            if(blocks.charAt(i)=='B') blacks++;
+            mini=Math.min(mini,k-blacks);
         }
         return mini;
+        // int idx=0;
+        // int mini=Integer.MAX_VALUE;
+        // while(idx+k<=blocks.length()){
+        //     int whites = check(idx,idx+k,blocks);
+        //     mini=Math.min(mini,whites);
+        //     idx++;
+        // }
+        // return mini;
     }
 }
