0377001af359d5c51446bc340927511c06a6ff7e
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-03-19 09:34:22 +0530

    Time: 7 ms (66.53%) | Memory: 57.1 MB (84.19%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,6 +1,7 @@
 class Solution {
     public int minOperations(int[] nums) {
         int cnt=0;
+        int n=nums.length;
         for(int i=0;i<nums.length-2;i++){
             if(nums[i]==0){
                 nums[i]=1;
@@ -9,7 +10,7 @@ class Solution {
                 cnt++;
             }
         }
-        for(int i=nums.length-3;i<nums.length;i++) if(nums[i]==0) return -1;
+        if(nums[n-1]==0 || nums[n-2]==0) return -1;
         return cnt;
     }
 }
