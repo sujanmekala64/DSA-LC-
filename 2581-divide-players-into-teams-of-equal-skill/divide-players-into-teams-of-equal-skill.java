2b0852597c4192f73fb3ef76750f2525a1d3f21a
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-03-26 09:30:49 +0530

    Time: 303 ms (5.53%) | Memory: 57 MB (84.17%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -6,12 +6,13 @@ class Solution {
         long val=skill[0]+skill[n-1];
         int m=skill[0]*skill[n-1];
         ans+=m;
-        for(int i=1;i<skill.length-1/2;i++){
+        for(int i=1;i<=(n-1)/2;i++){
             long add = skill[i]+skill[n-i-1];
             if(add!=val) return -1;
             long mul=skill[i]*skill[n-i-1];
             ans+=mul;
+            System.out.println(ans+" "+mul);
         }
-        return ans/2;
+        return ans;
     }
 }
