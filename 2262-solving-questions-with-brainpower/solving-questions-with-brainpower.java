e45ce47246d4c178da52f4402b54f77281c7bc2f
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-04-01 09:50:17 +0530

    Time: 12 ms (31.25%) | Memory: 125.6 MB (18.21%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long check(int questions[][],int idx,long dp[]){
+        if(idx>=questions.length) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        long take=questions[idx][0]+check(questions,idx+questions[idx][1]+1,dp);
+        long nottake=check(questions,idx+1,dp);
+        return dp[idx]=Math.max(take,nottake);
+    }
+    public long mostPoints(int[][] questions) {
+        long dp[]=new long[questions.length+1];
+        Arrays.fill(dp,-1);
+        return check(questions,0,dp);
+    }
+}
