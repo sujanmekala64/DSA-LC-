3af17ea33b63128447e7ebdac82eb51389f28c61
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-04-08 10:21:18 +0530

    Time: 4 ms (29.35%) | Memory: 44.4 MB (50.97%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,22 +1,39 @@
 class Solution {
-    public int minimumOperations(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(!map.containsKey(nums[i])) map.put(nums[i],1);
-            else map.put(nums[i],map.get(nums[i])+1);
+    public boolean check(int idx,int nums[]){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=idx+1;i<nums.length;i++){
+            if(set.contains(nums[i])) return false;
+            set.add(nums[i]);
         }
+        return true;
+    }
+    public int minimumOperations(int[] nums) {
         int ans=0;
-        if(map.size()==nums.length) return 0;
+        if(check(-1,nums)) return 0;
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])-1);
-                if(map.get(nums[i])==0) map.remove(nums[i]);
-            }
             if((i+1)%3==0){
                 ans++;
-                if(map.size()==nums.length-i-1) return ans;
+                if(check(i,nums)) return ans;
             }
         }
         return ans+1;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(!map.containsKey(nums[i])) map.put(nums[i],1);
+        //     else map.put(nums[i],map.get(nums[i])+1);
+        // }
+        // int ans=0;
+        // if(map.size()==nums.length) return 0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i],map.get(nums[i])-1);
+        //         if(map.get(nums[i])==0) map.remove(nums[i]);
+        //     }
+        //     if((i+1)%3==0){
+        //         ans++;
+        //         if(map.size()==nums.length-i-1) return ans;
+        //     }
+        // }
+        // return ans+1;
     }
 }
