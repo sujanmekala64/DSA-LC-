b18c2531fbc94bd6e0816fefe83d74051b11aa2c
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-04-13 14:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (27.56%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,12 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double res=1;
+        long m=Math.abs((long)n);
+        while(m>0){
+            if(m%2==1) res=res*x;
+            m=m/2;
+            x=x*x;
+        }
+        return n<0?1/res:res;
     }
 }
