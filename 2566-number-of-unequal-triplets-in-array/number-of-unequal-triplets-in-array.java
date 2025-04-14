dc5471a71788af82cf11ccc2edcf6cff3f6a2821
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-04-14 07:06:25 +0530

    Time: 7 ms (72.29%) | Memory: 41.3 MB (70.05%) - LeetSync

--- 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.java
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.java
@@ -3,11 +3,10 @@ class Solution {
         int ans=0;
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]) continue;
                 for(int k=j+1;k<nums.length;k++){
-                    if(nums[i]!=nums[j]){
-                        if(nums[j]!=nums[k]){
-                            if(nums[k]!=nums[i]) ans++;
-                        }
+                    if(nums[j]!=nums[k]){
+                        if(nums[k]!=nums[i]) ans++;
                     }
                 }
             }
