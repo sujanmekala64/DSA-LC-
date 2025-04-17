d7ff96369b9105136a6d019630529d3770984744
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-04-17 11:36:33 +0530

    Time: 5 ms (11.58%) | Memory: 42.9 MB (20.90%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
@@ -15,6 +15,11 @@ class Solution {
                 map.put(nums[i],arr);
             }
         }
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]==nums[j]) if((i*j)%k==0) ans++; 
+        //     }
+        // }
         return ans;
     }
 }
