8e68f2f5916d0af60c3d5f9086f305c0544efb72
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-04-17 11:34:22 +0530

    Time: 4 ms (16.39%) | Memory: 42.9 MB (28.87%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
@@ -1,9 +1,18 @@
 class Solution {
     public int countPairs(int[] nums, int k) {
         int ans=0;
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]) if((i*j)%k==0) ans++; 
+            if(!map.containsKey(nums[i])){
+                List<Integer> arr = new ArrayList<>();
+                arr.add(i);
+                map.put(nums[i],arr);
+            }
+            else{
+                List<Integer> arr = map.get(nums[i]);
+                for(int j=0;j<arr.size();j++) if((arr.get(j)*i)%k==0) ans++;
+                arr.add(i);
+                map.put(nums[i],arr);
             }
         }
         return ans;
