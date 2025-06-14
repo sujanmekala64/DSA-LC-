77ca73811919f579fab85dcf07499e39cc2d0a3b
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-06-14 20:35:27 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (11.84%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -1,8 +1,9 @@
 class Solution {
-    public int check(int val){
+    public int check(int val,int mini){
         int sum=0;
         while(val>0){
             sum+=val%10;
+            if(sum>mini) return Integer.MAX_VALUE;
             val/=10;
         }
         return sum;
@@ -10,7 +11,7 @@ class Solution {
     public int minElement(int[] nums) {
         int mini=Integer.MAX_VALUE;
         for(int val:nums){
-            int ret = check(val);
+            int ret = check(val,mini);
             if(ret<mini) mini=ret;
         }
         return mini;
