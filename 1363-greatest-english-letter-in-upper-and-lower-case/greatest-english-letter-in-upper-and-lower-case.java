89fd37c5801e3b57953db14ac0d7f6fbe05e2cf3
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-06-16 12:34:52 +0530

    Time: 2 ms (86.14%) | Memory: 42.5 MB (27.99%) - LeetSync

--- 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
@@ -1,10 +1,7 @@
 class Solution {
     public String greatestLetter(String s) {
         int arr[] = new int[123];
-        for(char p:s.toCharArray()){
-            int ascii = (int)(p);
-            arr[ascii]++;
-        }
+        for(char p:s.toCharArray())  arr[(int)(p)]++;
         String ans="";
         for(int i=64;i<=90;i++){
             if(arr[i]>0 && arr[i+32]>0) ans=((char)(i))+"";
