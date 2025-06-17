6168eb5d35d14d622eb10b93884405b6f70c807a
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-06-17 20:13:51 +0530

    Time: 50 ms (5.36%) | Memory: 44.7 MB (42.06%) - LeetSync

---
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            int val1 = nums.get(i).get(0);
+            int val2 = nums.get(i).get(1);
+            if(val1>maxi) maxi=val1;
+            if(val2>maxi) maxi=val2;
+        }
+        int diff[] = new int[maxi+2];
+        for(int i=0;i<nums.size();i++){
+            int val1 = nums.get(i).get(0);
+            int val2 = nums.get(i).get(1);
+            diff[val1]++;
+            diff[val2+1]--;
+        }
+        int arr[] = new int[maxi+2];
+        arr[0]=diff[0];
+        for(int i=1;i<maxi+2;i++){
+            diff[i]+=diff[i-1];
+        }
+        int ans=0;
+        for(int i=0;i<=maxi;i++){
+            System.out.println(i+" "+diff[i]);
+            if(diff[i]>0) ans++;
+        }
+        return ans;
+    }
+}
