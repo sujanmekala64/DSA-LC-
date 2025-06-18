9a56f041f67068966408bd470755425a8be3aa95
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-06-18 08:55:00 +0530

    Time: 23 ms (81.15%), Space: 59.4 MB (75.92%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -4,12 +4,8 @@ class Solution {
         Arrays.sort(nums);
         int idx=0;
         for(int i=0;i<nums.length/3;i++){
-            for(int j=0;j<3;j++){
-                if(j==0 && nums[idx+2]-nums[idx]>k){
-                    return new int[][]{};
-                }
-                ans[i][j]=nums[idx++];
-            }
+            if(nums[idx+2]-nums[idx]>k) return new int[][]{};
+            for(int j=0;j<3;j++) ans[i][j]=nums[idx++];
         }
         return ans;
     }
