079b80d8015f5d550c3d6ad29dd60b9f58d6511a
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-06 07:17:41 +0530

    Time: 1 ms (98.07%), Space: 40.9 MB (84.97%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,17 +1,13 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        List<Integer> li = new ArrayList<>();
-        for(int i=0;i<stones.length;i++) li.add(stones[i]);
-        Collections.sort(li);
-        while(li.size()>1){
-            int a = li.remove(li.size()-1);
-            int b = li.remove(li.size()-1);
-            int ans = Math.abs(a-b);
-            if(ans!=0){
-                li.add(ans);
-                Collections.sort(li);
-            }
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num:stones) queue.offer(num);
+        while(queue.size()>1){
+            int max=queue.poll();
+            int smax=queue.poll();
+            int num=max-smax;
+            if(num!=0) queue.offer(num);
         }
-        return li.size()>=1?li.get(0):0;
+        return queue.size()==1?queue.poll():0;
     }
 }
