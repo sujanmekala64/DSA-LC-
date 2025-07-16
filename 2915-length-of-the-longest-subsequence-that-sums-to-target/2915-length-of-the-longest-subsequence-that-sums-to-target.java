ec0a336230d0b811fa25fdf5236cb1e1698324b4
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-16 11:51:55 +0530

    Time: 309 ms (23.78%), Space: 55.2 MB (32.94%) - LeetHub

---
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subsequence(List<Integer> nums,int idx,int target,int dp[][]){
+        if(target==0) return 0;
+        if(target<0 || idx>=nums.size()) return Integer.MIN_VALUE;
+        if(dp[idx][target]!=-1) return dp[idx][target];
+        int nottake = subsequence(nums,idx+1,target,dp);
+        int take = 1+subsequence(nums,idx+1,target-nums.get(idx),dp);
+        dp[idx][target]=Math.max(nottake,take);
+        return dp[idx][target];
+    }
+    public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+        int dp[][] = new int[nums.size()+1][target+1];
+        for(int i=0;i<=nums.size();i++){
+            for(int j=0;j<=target;j++){
+                dp[i][j]=-1;
+            }
+        }
+        int ans = subsequence(nums,0,target,dp);
+        if(ans<0) return -1;
+        return ans;
+    }
+}
