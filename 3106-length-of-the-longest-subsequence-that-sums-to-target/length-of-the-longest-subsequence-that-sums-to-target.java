21473152d3ce1353c9c05a76f1bb6e59d565a26f
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-16 11:52:58 +0530

    Time: 315 ms (20.66%) | Memory: 55.2 MB (31.38%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,8 +1,8 @@
 class Solution {
     public int subsequence(List<Integer> nums,int idx,int target,int dp[][]){
         if(target==0) return 0;
-        if(target<0 || idx>=nums.size()) return Integer.MIN_VALUE;
-        if(dp[idx][target]!=-1) return dp[idx][target];
+        else if(target<0 || idx>=nums.size()) return Integer.MIN_VALUE;
+        else if(dp[idx][target]!=-1) return dp[idx][target];
         int nottake = subsequence(nums,idx+1,target,dp);
         int take = 1+subsequence(nums,idx+1,target-nums.get(idx),dp);
         dp[idx][target]=Math.max(nottake,take);
