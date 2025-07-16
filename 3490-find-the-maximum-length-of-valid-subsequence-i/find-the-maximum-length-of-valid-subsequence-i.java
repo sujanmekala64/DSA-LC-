a0fb1424df7ba1e33a1bc14bb32b4a713d96da06
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-16 11:30:52 +0530

    Time: 4 ms (86.07%) | Memory: 62 MB (84.43%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -5,12 +5,12 @@ class Solution {
         int flag=nums[0]%2;
         int both=1;
         for(int num:nums){
-            if(num%2==0) even++;
-            else if(num%2==1) odd++;
             if(num%2!=flag){
                 flag=1-flag;
                 both++;
             }
+            if(num%2==0) even++;
+            else odd++;
         }
         return Math.max(both,Math.max(even,odd));
     }
