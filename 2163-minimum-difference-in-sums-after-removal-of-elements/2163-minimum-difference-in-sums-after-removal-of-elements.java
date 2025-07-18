86c271fa788decc13f21b57b9f4542deb6f786b8
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-18 20:57:31 +0530

    Time: 162 ms (69.57%), Space: 82.3 MB (52.17%) - LeetHub

--- 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.java
+++ 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.java
@@ -1,26 +1,33 @@
 class Solution {
-    long ans=Integer.MAX_VALUE;
-    public void check(int nums[],int idx,List<Integer> li,int size){
-        if(li.size()!=0 && li.size()==size){
-            System.out.println(li);
-            long val=0;
-            for(int i=0;i<li.size()/2;i++) val+=li.get(i);
-            for(int i=li.size()/2;i<li.size();i++) val-=li.get(i);
-            ans=Math.min(ans,val);
-            return ;
-        }
-        if(idx>=nums.length) return ;
-        li.add(nums[idx]);
-        check(nums,idx+1,li,size);
-        // size--;
-        li.remove(li.size()-1);
-        check(nums,idx+1,li,size);
-        return ;
-    }
     public long minimumDifference(int[] nums) {
-        int n=nums.length/3;
-        int size=nums.length-n;
-        check(nums,0,new ArrayList<>(),size);
-        return ans;
+        int n = nums.length;
+        int k = n / 3;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long rightSum = 0;
+        long[] rightMaxSum = new long[n];
+        for (int i = n - 1; i >= k; i--) {
+            minHeap.offer(nums[i]);
+            rightSum += nums[i];
+            if (minHeap.size() > k) {
+                rightSum -= minHeap.poll();
+            }
+            if (minHeap.size() == k) {
+                rightMaxSum[i] = rightSum;
+            }
+        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        long leftSum = 0;
+        long minDiff = Long.MAX_VALUE;
+        for (int i = 0; i < 2 * k; i++) {
+            maxHeap.offer(nums[i]);
+            leftSum += nums[i];
+            if (maxHeap.size() > k) {
+                leftSum -= maxHeap.poll();
+            }
+            if (maxHeap.size() == k && i + 1 < n) {
+                minDiff = Math.min(minDiff, leftSum - rightMaxSum[i + 1]);
+            }
+        }
+        return minDiff;
     }
 }
