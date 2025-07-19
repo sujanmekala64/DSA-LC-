467306d9e83a39aa65cff0ed3adb29e7090f5256
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-19 10:01:30 +0530

    Time: 323 ms (15.49%) | Memory: 96.9 MB (7.75%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.java
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.java
@@ -1,33 +1,38 @@
 class Solution {
     public long minimumDifference(int[] nums) {
-        int n = nums.length;
-        int k = n / 3;
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        long rightSum = 0;
-        long[] rightMaxSum = new long[n];
-        for (int i = n - 1; i >= k; i--) {
-            minHeap.offer(nums[i]);
-            rightSum += nums[i];
-            if (minHeap.size() > k) {
-                rightSum -= minHeap.poll();
+        int n=nums.length/3;
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+        long ls=0;
+        long rs=0;
+        long leftsum[] = new long[nums.length];
+        long rightsum[] = new long[nums.length];
+        for(int i=0;i<nums.length;i++){
+            maxheap.offer(nums[i]);
+            ls+=nums[i];
+            if(maxheap.size()>n){
+                ls-=maxheap.poll();
             }
-            if (minHeap.size() == k) {
-                rightMaxSum[i] = rightSum;
+            if(maxheap.size()==n){
+                leftsum[i]=ls;
+            }
+            else leftsum[i]=Long.MAX_VALUE;
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            minheap.offer(nums[i]);
+            rs+=nums[i];
+            if(minheap.size()>n){
+                rs-=minheap.poll();
+            }
+            if(minheap.size()==n) rightsum[i]=rs;
+            else rightsum[i]=Long.MIN_VALUE;
+        }
+        long ans=Long.MAX_VALUE;
+        for(int i=n-1;i<nums.length-n;i++){
+            if(leftsum[i]!=Long.MAX_VALUE && rightsum[i+1]!=Long.MIN_VALUE){
+                ans=Math.min(ans,leftsum[i]-rightsum[i+1]);
             }
         }
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        long leftSum = 0;
-        long minDiff = Long.MAX_VALUE;
-        for (int i = 0; i < 2 * k; i++) {
-            maxHeap.offer(nums[i]);
-            leftSum += nums[i];
-            if (maxHeap.size() > k) {
-                leftSum -= maxHeap.poll();
-            }
-            if (maxHeap.size() == k && i + 1 < n) {
-                minDiff = Math.min(minDiff, leftSum - rightMaxSum[i + 1]);
-            }
-        }
-        return minDiff;
+        return ans;
     }
 }
