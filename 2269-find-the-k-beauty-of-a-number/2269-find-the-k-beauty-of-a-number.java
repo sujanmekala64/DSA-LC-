d1dba4a2d20d411c0f3fa333194176402862b8ed
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-22 17:33:13 +0530

    Time: 8 ms (5.11%), Space: 41.2 MB (7.11%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        String n = String.valueOf(num);
+        String s="";
+        for(int i=0;i<k;i++) s+=n.charAt(i);
+        int ans=0;
+        int val=Integer.valueOf(s);
+        if(num%val==0) ans++;
+        for(int i=k;i<n.length();i++){
+            s = s.substring(1,k)+(n.charAt(i));
+            val=Integer.valueOf(s);
+            if(val!=0 && num%val==0) ans++;
+        }
+        return ans;
+    }
+}
