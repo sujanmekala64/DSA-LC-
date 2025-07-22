a59ba1ceb5afcc276451e0ab8b429b18ae84dace
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-22 17:34:10 +0530

    Time: 7 ms (5.11%), Space: 41.4 MB (7.11%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int divisorSubstrings(int num, int k) {
+    public int divisorSubstrings(int num, int k) {  //can be done using substring but donw with sliding window
         String n = String.valueOf(num);
         String s="";
         for(int i=0;i<k;i++) s+=n.charAt(i);
