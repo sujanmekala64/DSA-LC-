4affdfd1bf00321f89c9018e05364042eaad6387
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-28 16:46:36 +0530

    Time: 64 ms (27.51%), Space: 66.5 MB (5.54%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,5 +1,5 @@
 class Solution {
-    // public int findMaxXor(int nums[],int idx,int val){
+    // public int findMaxor(int nums[],int idx,int val){
     //     if(idx==nums.length){
     //         return val;
     //     }
@@ -7,19 +7,23 @@ class Solution {
     //     int inc = findMaxXor(nums,idx+1,val|nums[idx]);
     //     return Math.max(notinc,inc);
     // }
-    public int findCount(int nums[],int idx,int val,int req){
+    public int findCount(int nums[],int idx,int val,int req,int dp[][]){
+        if(val>req) return 0;
         if(idx==nums.length){
             if(val==req) return 1;
             return 0;
         }
-        int notinc = findCount(nums,idx+1,val,req);
-        int inc = findCount(nums,idx+1,val|nums[idx],req);
-        return notinc+inc;
+        if(dp[idx][val]!=-1) return dp[idx][val];
+        int notinc = findCount(nums,idx+1,val,req,dp);
+        int inc = findCount(nums,idx+1,val|nums[idx],req,dp);
+        return dp[idx][val]=notinc+inc;
     }
     public int countMaxOrSubsets(int[] nums) {
         // int maxxor=findMaxXor(nums,0,0);
         int maxor=0;
         for(int num:nums) maxor|=num;
-        return findCount(nums,0,0,maxor);
+        int dp[][] = new int[nums.length+1][maxor+1];
+        for(int i=0;i<=nums.length;i++) for(int j=0;j<=maxor;j++) dp[i][j]=-1;
+        return findCount(nums,0,0,maxor,dp);
     }
 }
