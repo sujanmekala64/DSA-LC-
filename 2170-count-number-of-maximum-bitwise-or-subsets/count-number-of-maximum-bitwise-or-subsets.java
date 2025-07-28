da58788775ddc4f07c860c53aba2b76388cef447
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-07-28 16:42:21 +0530

    Time: 16 ms (32.41%) | Memory: 41.4 MB (46.70%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,5 +1,4 @@
 class Solution {
-    int ans=0;
     public int findMaxXor(int nums[],int idx,int val){
         if(idx==nums.length){
             return val;
@@ -8,18 +7,17 @@ class Solution {
         int inc = findMaxXor(nums,idx+1,val|nums[idx]);
         return Math.max(notinc,inc);
     }
-    public void findCount(int nums[],int idx,int val,int req){
+    public int findCount(int nums[],int idx,int val,int req){
         if(idx==nums.length){
-            if(val==req) ans++;
-            return ;
+            if(val==req) return 1;
+            return 0;
         }
-        findCount(nums,idx+1,val,req);
-        findCount(nums,idx+1,val|nums[idx],req);
-        return ;
+        int notinc = findCount(nums,idx+1,val,req);
+        int inc = findCount(nums,idx+1,val|nums[idx],req);
+        return notinc+inc;
     }
     public int countMaxOrSubsets(int[] nums) {
         int maxxor=findMaxXor(nums,0,0);
-        findCount(nums,0,0,maxxor);
-        return ans;
+        return findCount(nums,0,0,maxxor);
     }
 }
