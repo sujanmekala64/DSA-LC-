4712bb8e8e37bf37234920594d23ce18a8cdf675
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-01 14:30:40 +0530

    Time: 1 ms (13.59%), Space: 41.8 MB (63.04%) - LeetHub

--- 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.java
+++ 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canMakeSquare(char[][] grid) {
+        int rows[] = {0,0,1,1};
+        int cols[] = {0,1,0,1};
         for(int i=0;i<grid.length-1;i++){
             for(int j=0;j<grid[0].length-1;j++){
-                int rows[] = {0,0,1,1};
-                int cols[] = {0,1,0,1};
                 int w=0;
                 int b=0;
                 for(int k=0;k<4;k++){
