8589e9859aa84e17682ae02366e3f6d339915f03
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-08 12:04:23 +0530

    Time: 56 ms (13.77%), Space: 224.4 MB (16.77%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.java
+++ 0808-soup-servings/0808-soup-servings.java
@@ -3,7 +3,7 @@ class Solution {
         if(A<=0 && B>0) return 1.0;
         else if(A<=0 && B<=0) return 0.5;
         else if(A>0 && B<=0) return 0.0;
-        if(dp[A][B]!=-1) return dp[A][B];
+        if(dp[A][B]!=-1.0) return dp[A][B];
         return dp[A][B]=0.25*(checkProb(A-100,B,dp)+checkProb(A-75,B-25,dp)+checkProb(A-50,B-50,dp)+checkProb(A-25,B-75,dp));
     }
     public double soupServings(int n) {
