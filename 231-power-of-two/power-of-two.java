07b780f62e908d44ef934c1e9b96b47d7f889f8f
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-09 11:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (79.73%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        while(n>=2 && n%2==0) n/=2;
+        if(n==0) return false;
+        while(n%2==0) n/=2;
         return n==1;
     }
 }
