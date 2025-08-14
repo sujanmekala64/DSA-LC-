9d89a0bcbc3b56554ca5dd8093e980f5cd352918
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-14 07:21:50 +0530

    Time: 2 ms (81.68%), Space: 41.7 MB (96.39%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -1,16 +1,31 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        int ans=-1;
-        String ret="";
-        for(int i=1;i<num.length()-1;i++){
-            if(num.charAt(i)==num.charAt(i-1) && num.charAt(i)==num.charAt(i+1)){
-                int val=num.charAt(i)-'0';
-                if(val>ans){
-                    ans=val;
-                    ret=String.valueOf(num.charAt(i)).repeat(3);
-                }
-            }
+        String f="";
+        int n=num.length();
+        for(int i=1;i<n-1;i++){
+            if(num.charAt(i)==num.charAt(i-1) && num.charAt(i)==num.charAt(i+1))
+                if(num.substring(i-1,i+2).compareTo(f)>0)
+                    f=num.substring(i-1,i+2);
         }
-        return ret;
+        return f;
     }
 }
+// int n = num.length(),max=Integer.MIN_VALUE,f=Integer.MIN_VALUE;
+//         String h = " ";
+//         char p[] = num.toCharArray();
+//         int nums[] = new int[n];
+//         for(int i=0;i<n;i++){
+//             nums[i] = Character.getNumericValue(p[i]);
+//         }
+//         for(int i=1;i<n-1;i++){
+//             if(nums[i-1]==nums[i] && nums[i]==nums[i+1]){
+//                 max = nums[i];
+//             }
+//             if(max>f){
+//                 f=max;
+//                 h = Integer.toString(nums[i-1])+Integer.toString(nums[i])+Integer.toString(nums[i+1]);
+//             }
+//         }
+//         if(f==Integer.MIN_VALUE)
+//             return "";
+//         return h;
