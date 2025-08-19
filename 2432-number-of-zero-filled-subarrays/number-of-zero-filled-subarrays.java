db925d3f85e0c840e82f49613c9bddc981eb6877
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-19 06:47:05 +0530

    Time: 3 ms (94.03%) | Memory: 62.3 MB (19.96%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long ans=0;
+        long cnt=1;
+        for(int num:nums){
+            if(num==0){
+                ans+=cnt;
+                cnt++;
+            }
+            else cnt=1;
+        }
+        return ans;
+    }
+}
