12fa46a6509fdb72061772e47de9faaa41493861
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-25 12:49:23 +0530

    Time: 1015 ms (5.8%), Space: 258.9 MB (11.59%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        int cols = encodedText.length()/rows;
+        TreeMap<Integer,List<Character>> map = new TreeMap<>();
+        char mat[][] = new char[rows][cols];
+        int r=0;
+        int c=0;
+        for(int i=0;i<encodedText.length();i++){
+            if(c==cols){
+                r++;
+                c=0;
+            }
+            mat[r][c]=encodedText.charAt(i);
+            c++;
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                int val = (j-i);
+                if(!map.containsKey(val)) map.put(val,new ArrayList<>());
+                map.get(val).add(mat[i][j]);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int key:map.keySet()){
+            if(key<0) continue;
+            List<Character> li = map.get(key);
+            for(int i=0;i<li.size();i++) sb.append(li.get(i));
+        }
+        for(int i=sb.length()-1;i>=0;i--){
+            if(sb.charAt(i)==' ') sb.deleteCharAt(i);
+            else break;
+        }
+        return sb.toString();
+    }
+}
