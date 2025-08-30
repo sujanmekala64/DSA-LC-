693f63e988de9f4be76f8a95cea7bbcd2652f438
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-08-30 18:41:58 +0530

    Time: 1 ms (99.87%), Space: 44.6 MB (48.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,44 +1,44 @@
 class Solution {
     boolean ans=true;
-    public boolean isSafe(char board[][],int row,int col,int number){
+    public boolean isSafe(char board[][],int r,int c,char number){
         //horizontal
-        for(int i=0;i<board.length;i++){
-            if(i!=col && board[row][i]==number) return false;
+        for(int i=0;i<9;i++){
+            if(i!=c && board[r][i]==number) return false;
         }
         //vertical
-        for(int i=0;i<board.length;i++){
-            if(i!=row && board[i][col]==number) return false;
+        for(int i=0;i<9;i++){
+            if(i!=r && board[i][c]==number) return false;
         }
-        //grid check
-        int startrow = (row/3)*3;
-        int startcol = (col/3)*3;
+        int startrow = (r/3)*3;
+        int startcol = (c/3)*3;
         for(int i=startrow;i<startrow+3;i++){
             for(int j=startcol;j<startcol+3;j++){
-                if(i!=row && j!=col && board[i][j]==number) return false;
+                if(i!=r && j!=c && board[i][j]==number) return false;
             }
         }
         return true;
     }
     public void helper(char board[][],int row,int col){
-        if(row==board.length){
-            return ;
-        }
+        if(col==9) return ;
         int nrow=0;
         int ncol=0;
-        if(col!=board.length-1){
-            nrow=row;
-            ncol=col+1;
+        if(row<8){
+            nrow=row+1;
+            ncol=col;
         }
         else{
-            nrow=row+1;
-            ncol=0;
+            nrow=0;
+            ncol=col+1;
         }
         if(board[row][col]!='.'){
             if(isSafe(board,row,col,board[row][col])){
                 helper(board,nrow,ncol);
             }
-            else ans=false;
-        }else helper(board,nrow,ncol);
+            else{
+                ans=false;
+            }
+        }
+        else helper(board,nrow,ncol);
     }
     public boolean isValidSudoku(char[][] board) {
         helper(board,0,0);
