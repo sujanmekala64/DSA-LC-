85175731a7171f6d9a8d695790d36ca4f4c56f5b
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-09-12 10:53:07 +0530

    Time: 4 ms (79.79%) | Memory: 45.4 MB (82.68%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int cnt=0;
         for(char p:s.toCharArray()){
-            if(p=='a' || p=='e' || p=='i' || p=='o' || p=='u') cnt++;
+            if(p=='a' || p=='e' || p=='i' || p=='o' || p=='u') return true;
         }
-        if(cnt==0) return false;
-        return true;
+        return false;
     }
 }
