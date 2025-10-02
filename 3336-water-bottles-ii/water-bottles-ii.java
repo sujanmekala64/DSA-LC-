eba8b5b80cca34a902ce139e019494866aa7847f
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-10-02 07:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (9.68%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int empty=numBottles;
+        int drunk=numBottles;
+        numBottles=0;
+        int cnt=1;
+        while(true){
+            if(empty<numExchange){
+                drunk+=numBottles;
+                empty+=numBottles;
+                numBottles=0;
+                if(empty<numExchange) break;
+            }
+            else{
+                empty=empty-numExchange;
+                numExchange++;
+                numBottles++;
+            }
+        }
+        return drunk;
+    }
+}
