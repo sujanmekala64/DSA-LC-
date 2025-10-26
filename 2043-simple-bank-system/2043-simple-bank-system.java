c309d93a2d8f10cb8bad01404c31883c29e700a7
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-10-26 15:10:44 +0530

    Time: 97 ms (68.95%), Space: 101.4 MB (37.74%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.java
+++ 2043-simple-bank-system/2043-simple-bank-system.java
@@ -1,27 +1,36 @@
 class Bank {
-    long balance[];
+    long[] balance;
     public Bank(long[] balance) {
-        this.balance=balance;
+        this.balance = balance;
     }
     
     public boolean transfer(int account1, int account2, long money) {
-        if(account1>balance.length || account2>balance.length) return false;
-        if(balance[account1-1]<money) return false;
-        balance[account1-1]-=money;
-        balance[account2-1]+=money;
-        return true;
+        if(isAccountValid(account1) && isAccountValid(account2) && balance[account1-1]>=money){
+            balance[account1-1] = balance[account1-1]-money;
+            balance[account2-1] = balance[account2-1]+money;
+            return true;
+        }
+        return false;
     }
     
     public boolean deposit(int account, long money) {
-        if(account>balance.length) return false;
-        balance[account-1]+=money;
-        return true;
+        if(isAccountValid(account)){
+            balance[account-1] = balance[account-1]+money;
+            return true;
+        }
+        return false;
     }
     
     public boolean withdraw(int account, long money) {
-        if(account>balance.length || balance[account-1]<money) return false;
-        balance[account-1]-=money;
-        return true;
+        if(isAccountValid(account) && balance[account-1]>=money){
+            balance[account-1] = balance[account-1]-money;
+            return true;
+        }
+        return false;
+    }
+    public boolean isAccountValid(int account){
+        if(account-1<balance.length && account>=1) return true;
+        return false;
     }
 }
 
