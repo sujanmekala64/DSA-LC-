dba424b978e5c7699bae90230c8160018a3bf295
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-10-27 19:46:12 +0530

    Time: 12 ms (53.73%) | Memory: 44.9 MB (95.32%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        List<Integer> li = new ArrayList<>();
+        for(String val:bank){
+            int cnt=0;
+            for(char p:val.toCharArray()){
+                if(p=='1') cnt++;
+            }
+            if(cnt!=0) li.add(cnt);
+        }
+        int ans=0;
+        if(li.size()>1){
+            for(int i=1;i<li.size();i++){
+                ans+=li.get(i-1)*li.get(i);
+            }
+            return ans;
+        }
+        return 0;
+    }
+}
