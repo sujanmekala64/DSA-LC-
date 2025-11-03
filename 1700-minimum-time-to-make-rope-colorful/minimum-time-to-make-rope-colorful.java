ad8e37e310962f9cd789743e0d2d7bfe5cd8c849
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-11-03 20:12:19 +0530

    Time: 32 ms (6.84%) | Memory: 85.2 MB (5.68%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,38 @@
+class Pair{
+    char c;
+    int val;
+    Pair(char c,int val){
+        this.c=c;
+        this.val=val;
+    }   
+}
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        Stack<Pair> st = new Stack<>();
+        int ans=0;
+        for(int i=0;i<colors.length();i++){
+            char p=colors.charAt(i);
+            int time=neededTime[i];
+            if(!st.isEmpty()){
+                Pair pair = st.peek();
+                if(pair.c==p){
+                    if(pair.val<=time){
+                        st.pop();
+                        ans+=pair.val;
+                        st.push(new Pair(p,time));
+                    }
+                    else{
+                        ans+=time;
+                    }
+                }
+                else{
+                    st.push(new Pair(p,time));
+                }
+            }
+            else{
+                st.push(new Pair(p,time));
+            }
+        }
+        return ans;
+    }
+}
