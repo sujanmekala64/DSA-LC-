c07b982c73aa1e78310b3e143eccef59a342e981
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-11-16 09:27:18 +0530

    Time: 5 ms (57.07%) | Memory: 46.3 MB (14.66%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -4,12 +4,9 @@ class Solution {
         int r=0;
         long ans=0;
         int mod=1_000_000_007;
-        boolean check=false;
         while(l<s.length()){
-            check=false;
             while(r<s.length() && s.charAt(r)=='1'){
                 r++;
-                check=true;
             }
             // if(c)
             if(l!=r){
