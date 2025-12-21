dfb63db9e4cc6a6e9b1b112c98602054f2ba8d6c
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2025-12-21 17:34:01 +0530

    Time: 1 ms (78.08%), Space: 43.5 MB (60.27%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
         int ans=0;
-        HashSet<Integer> set = new HashSet<>();
+        boolean sortedcol[] = new boolean[strs.length];
+        boolean check=false;
         for(int i=0;i<strs[0].length();i++){
-            for(int j=1;j<strs.length;j++){
-                if((!set.contains(j) || !set.contains(j-1)) && strs[j].charAt(i)<strs[j-1].charAt(i)){
+            check=false;
+            for(int j=0;j<strs.length-1;j++){
+                if(!sortedcol[j] && strs[j].charAt(i)>strs[j+1].charAt(i)){
+                    check=true;
                     ans++;
                     break;
                 }
-                else if(strs[j].charAt(i)>strs[j-1].charAt(i)){
-                    set.add(j);
-                    set.add(j-1);
-                }
             }
-            if(set.size()==strs.length) break;
+            if(check) continue;
+            for(int k=0;k<strs.length-1;k++){
+                sortedcol[k]=sortedcol[k]|(strs[k].charAt(i)<strs[k+1].charAt(i));
+            }
         }
         return ans;
     }
