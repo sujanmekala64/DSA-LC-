8439e54ac823148636a33b46d77fc29b3bbd9be2
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-01-04 10:39:53 +0530

    Time: 114 ms (11.87%) | Memory: 47.7 MB (5.30%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int check(int num){
+        int ans=0;
+        int sum=0;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i;
+                ans+=1;
+                if(i!=num/i){
+                    ans+=1;
+                    sum+=num/i;
+                }
+            }
+        }
+        System.out.println(num+" "+ans+" "+sum);
+        if(ans==4) return sum;
+        return 0;
+    }
+    public int sumFourDivisors(int[] nums) {
+        int ans=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            if(!map.containsKey(num)) map.put(num,check(num));
+            ans+=map.get(num);
+        }
+        return ans;
+    }
+}
