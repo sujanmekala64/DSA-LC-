f34711347e9c4581baa7b7eb695f60001abbd2d6
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-01-08 22:36:16 +0530

    Time: 12 ms (45.19%), Space: 46.5 MB (65.93%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,7 +1,7 @@
 class Solution {
     public int check(int nums1[],int nums2[],int idx1,int idx2,int m,int n,int dp[][]){
         if(idx1>=m || idx2>=n) return -1000000000;
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+        if(dp[idx1][idx2]!=Integer.MIN_VALUE) return dp[idx1][idx2];
         int first = nums1[idx1]*nums2[idx2];
         int callfirst = first+check(nums1,nums2,idx1+1,idx2+1,m,n,dp);
         int takei=check(nums1,nums2,idx1,idx2+1,m,n,dp);
@@ -12,7 +12,7 @@ class Solution {
         int dp[][] = new int[nums1.length+1][nums2.length+1];
         for(int i=0;i<=nums1.length;i++){
             for(int j=0;j<=nums2.length;j++){
-                dp[i][j]=-1;
+                dp[i][j]=Integer.MIN_VALUE;
             }
         }
         return check(nums1,nums2,0,0,nums1.length,nums2.length,dp);
