0a6868fa01e0bbe742acd09d5a5f1c356a698b9e
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-01-31 09:54:41 +0530

    Time: 1 ms (2.42%), Space: 46.7 MB (9.86%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int mini=Integer.MAX_VALUE;
+        int f=target-'a';
+        // int g=26-(target-'a');
+        char h='a';
+        for(char s:letters){
+            int g = s-'a';
+            if(g<f && s!=target && mini>26-f+g){
+                mini=26-f+g;
+                h=s;
+            }
+            if(g>f && s!=target && mini>(g-f)){
+                mini=g-f;
+                h=s;
+            }
+        }
+        return h;
+    }
+}
