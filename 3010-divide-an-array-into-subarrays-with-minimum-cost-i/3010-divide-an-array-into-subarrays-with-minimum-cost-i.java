b1420ffcf15c6e282d034f731ec46c7c14b37f36
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-02-01 20:00:02 +0530

    Time: 1 ms (99.2%), Space: 45.4 MB (72.8%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minimumCost(int[] nums) {
         int ans = nums[0];
-        int onemin=Math.min(nums[1],nums[2]);
-        int twomin=Math.max(nums[1],nums[2]);
+        int onemin=nums[1];
+        int twomin=nums[2];
         for(int i=3;i<nums.length;i++){
             if(nums[i]<=onemin || nums[i]<=twomin){
                 if(onemin>twomin){
