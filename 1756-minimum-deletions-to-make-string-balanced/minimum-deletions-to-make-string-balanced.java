f6cf2c318f7d5308b0d9ee66a0de3672110a3fdb
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-02-07 18:49:56 +0530

    Time: 36 ms (44.57%) | Memory: 48 MB (30.43%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int as=0;
+        for(char p:s.toCharArray()) if(p=='a') as++;
+        int bs=0;
+        int ans=Integer.MAX_VALUE;
+        for(char p:s.toCharArray()){
+            if(p=='a') as--;
+            ans=Math.min(ans,(as+bs));
+            if(p=='b') bs++;
+        }
+        return ans;
+    }
+}
