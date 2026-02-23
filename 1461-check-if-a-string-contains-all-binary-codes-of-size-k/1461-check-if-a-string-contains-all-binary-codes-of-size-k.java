c5196703a789258d3fd1478794ef192112eb43c9
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-02-23 16:39:01 +0530

    Time: 180 ms (32.35%), Space: 95.8 MB (68.89%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,23 +1,17 @@
 class Solution {
-    public void generateSubstrings(int k,List<String> ans,String s){
-        if(s.length()==k){
-            ans.add(s);
-            return ;
-        }
-        generateSubstrings(k,ans,s+'0');
-        generateSubstrings(k,ans,s+'1');
-    }
+    // public void generateSubstrings(int k,List<String> ans,String s){
+    //     if(s.length()==k){
+    //         ans.add(s);
+    //         return ;
+    //     }
+    //     generateSubstrings(k,ans,s+'0');
+    //     generateSubstrings(k,ans,s+'1');
+    // }
     public boolean hasAllCodes(String s, int k) {
-        List<String> ans = new ArrayList<>();
-        generateSubstrings(k,ans,"");
-        int idx=0;
         HashSet<String> set = new HashSet<>();
-        String substr="";
-        while(idx<=s.length()-k){
-            substr=s.substring(idx,k+idx);
-            if(!set.contains(substr) && ans.contains(substr)) set.add(substr);
-            idx++;
+        for(int i=0;i<=s.length()-k;i++){
+            set.add(s.substring(i,k+i));
         }
-        return set.size()==ans.size();
+        return set.size()==(int)Math.pow(2,k);
     }
 }
