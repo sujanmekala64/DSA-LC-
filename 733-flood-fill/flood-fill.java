96845930f1a430299df0692468a5c64788002066
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-03-25 10:27:40 +0530

    Time: 2 ms (13.44%) | Memory: 46.7 MB (86.83%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,37 @@
 class Solution {
-    public void check(int val,int sr,int sc,int color,int[][] image,int row,int col,boolean vis[][]){
-        if(sr<0 || sr>=row || sc<0 || sc>=col || image[sr][sc]==color || vis[sr][sc]) return ;
-        if(!vis[sr][sc] && image[sr][sc]==val){
-            vis[sr][sc]=true;
-            image[sr][sc]=color;
-            check(val,sr-1,sc,color,image,row,col,vis); //up
-            check(val,sr+1,sc,color,image,row,col,vis); //down
-            check(val,sr,sc-1,color,image,row,col,vis); //left
-            check(val,sr,sc+1,color,image,row,col,vis); //right
+    class Pair{
+        int row;
+        int col;
+        public Pair(int row,int col){
+            this.row=row;
+            this.col=col;
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc]==color) return image;
-        int val=image[sr][sc];
-        boolean vis[][] = new boolean[image.length][image[0].length];
-        check(val,sr,sc,color,image,image.length,image[0].length,vis);
+        Queue<Pair> queue = new LinkedList<>();
+        int r=image.length;
+        int c=image[0].length;
+        int vis[][] = new int[r][c];
+        int rows[] = {-1,1,0,0};
+        int cols[] = {0,0,-1,1};
+        queue.offer(new Pair(sr,sc));
+        int normal=image[sr][sc];
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            while(size-->0){
+                Pair p=queue.poll();
+                int row=p.row;
+                int col=p.col;
+                vis[row][col]=1;
+                image[row][col]=color;
+                for(int i=0;i<4;i++){
+                    int newrow=rows[i]+row;
+                    int newcol=cols[i]+col;
+                    if(newrow<0 || newrow>=r || newcol<0 || newcol>=c || vis[newrow][newcol]==1 || image[newrow][newcol]!=normal) continue;
+                    queue.offer(new Pair(newrow,newcol));
+                }
+            }
+        }
         return image;
     }
 }
