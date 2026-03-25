50aacf9cbdaca39acb0bb927026ca85fead74c7d
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-03-25 13:00:33 +0530

    Time: 8 ms (7.23%), Space: 46.8 MB (95.73%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,22 +1,31 @@
 class Solution {
-    public boolean checkBiPartite(List<List<Integer>> ans,int vis[],int start){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        vis[start]=0;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            while(size-->0){
-                int val = queue.poll();
-                int color=vis[val];
-                List<Integer> li = ans.get(val);
-                for(int i=0;i<li.size();i++){
-                    if(vis[li.get(i)]==-1){
-                        queue.offer(li.get(i));
-                        vis[li.get(i)]=1-color;
-                    }
-                    else if(vis[li.get(i)]==vis[val]) return false;
-                }
+    public boolean checkBiPartite(List<List<Integer>> ans,int vis[],int start,int color){
+        // Queue<Integer> queue = new LinkedList<>();
+        // queue.offer(start);
+        // vis[start]=0;
+        // while(!queue.isEmpty()){
+        //     int size=queue.size();
+        //     while(size-->0){
+        //         int val = queue.poll();
+        //         int color=vis[val];
+        //         List<Integer> li = ans.get(val);
+        //         for(int i=0;i<li.size();i++){
+        //             if(vis[li.get(i)]==-1){
+        //                 queue.offer(li.get(i));
+        //                 vis[li.get(i)]=1-color;
+        //             }
+        //             else if(vis[li.get(i)]==vis[val]) return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        vis[start]=color;
+        List<Integer> li = ans.get(start);
+        for(int i=0;i<li.size();i++){
+            if(vis[li.get(i)]==-1){
+                if(!checkBiPartite(ans,vis,li.get(i),1-color)) return false;
             }
+            else if(vis[li.get(i)]==vis[start]) return false;
         }
         return true;
     }
@@ -32,7 +41,7 @@ class Solution {
         Arrays.fill(vis,-1);
         for(int i=0;i<graph.length;i++){
             if(vis[i]==-1){
-                if(!checkBiPartite(ans,vis,i)) return false;
+                if(!checkBiPartite(ans,vis,i,0)) return false;
             }
         }
         return true;
