2de7d6942f37878f208b19ce4bbb3073cad87ffc
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-04-15 11:16:52 +0530

    Time: 4 ms (100.00%) | Memory: 84.6 MB (57.63%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int rem[] = new int[k];
+        for(int num:arr){
+            rem[((num%k)+k)%k]++;
+        }
+        if(rem[0]%2==1) return false;
+        for(int i=1;i<=k/2;i++){
+
+            if(rem[i]!=rem[k-i]) return false;
+        }
+        return true;
+    }
+}
