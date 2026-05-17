25dfe040f2d6c9a70291a0ec0a1ad7a0cfb44e18
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-05-17 12:37:41 +0530

    Time: 15 ms (8.18%) | Memory: 50.3 MB (61.39%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,14 +1,35 @@
 class Solution {
-    public boolean check(int arr[],int start,HashSet<Integer> visited){
-        if(start<0 || start>=arr.length) return false;
-        if(visited.contains(start))  return false;
-        if(arr[start]==0) return true;
-        visited.add(start);
-        boolean forward = check(arr,start+arr[start],visited);
-        boolean backward = check(arr,start-arr[start],visited);
-        return forward||backward;
-    }
+    // public boolean check(int arr[],int start,HashSet<Integer> visited){
+    //     if(start<0 || start>=arr.length) return false;
+    //     if(visited.contains(start))  return false;
+    //     if(arr[start]==0) return true;
+    //     visited.add(start);
+    //     boolean forward = check(arr,start+arr[start],visited);
+    //     boolean backward = check(arr,start-arr[start],visited);
+    //     return forward||backward;
+    // }
     public boolean canReach(int[] arr, int start){
-        return check(arr,start,new HashSet<>());
+        // return check(arr,start,new HashSet<>());
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        HashSet<Integer>  visited = new HashSet<>();
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            while(size-->0){
+                int val=queue.poll();
+                if(arr[val]==0) return true;
+                int front=val+arr[val];
+                int back=val-arr[val];
+                if(front<arr.length && !visited.contains(front)){
+                    queue.add(front);
+                    visited.add(front);
+                }
+                if(back>=0 && !visited.contains(back)){
+                    queue.add(back);
+                    visited.add(back);
+                }
+            }
+        }
+        return false;
     }
 }
