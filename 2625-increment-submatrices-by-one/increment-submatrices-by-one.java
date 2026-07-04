55f389c79a45e87ec56f40895acd351741c6c9e6
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-07-04 16:04:07 +0530

    Time: 30 ms (32.22%) | Memory: 81.6 MB (97.41%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int diff[][] = new int[n][n];
-        for(int i=0;i<queries.length;i++){
-            int r1=queries[i][0];
-            int c1=queries[i][1];
-            int r2=queries[i][2];
-            int c2=queries[i][3];
-            for(int j=r1;j<=r2;j++){
-                diff[j][c1]+=1;
-                if(c2+1<n) diff[j][c2+1]-=1;
+        int diff[][] = new int[n+1][n+1];
+        for(int query[]:queries){
+            int r1=query[0];
+            int c1=query[1];
+            int r2=query[2];
+            int c2=query[3];
+            for(int i=r1;i<=r2;i++){
+                diff[i][c1]++;
+                diff[i][c2+1]--;
             }
         }
+        int ans[][] = new int[n][n];
         for(int i=0;i<n;i++){
+            ans[i][0]=diff[i][0];
             for(int j=1;j<n;j++){
-                diff[i][j]=diff[i][j]+diff[i][j-1];
+                ans[i][j]=diff[i][j-1]+diff[i][j];
+                diff[i][j]=ans[i][j];
             }
         }
-        return diff;
+        return ans;
     }
 }
