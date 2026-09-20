5051d955cc0d8891cb810adad7e835f39f204ce9
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-09-20 14:57:13 +0530

    Time: 7 ms (14.58%) | Memory: 46.9 MB (60.67%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
@@ -1,23 +1,34 @@
 class Solution {
     public int countIntersectingIntervals(int[][] intervals) {
-        List<int[]> li = new ArrayList<>();
-        for(int inte[]:intervals){
-            li.add(new int[]{inte[0],1});
-            li.add(new int[]{inte[1],-1});
-        }
-        Collections.sort(li,(a,b)->{
+        int ans=0;
+        Arrays.sort(intervals,(a,b)->{
             if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
             return Integer.compare(b[1],a[1]);
         });
-        int ans=0;
-        int cnt=0;
-        for(int val[]:li){
-            if(val[1]==1){
-                ans+=cnt;
-                cnt++;
+        for(int i=0;i<intervals.length;i++){
+            for(int j=i+1;j<intervals.length;j++){
+                if(intervals[j][0]<=intervals[i][1] && intervals[i][0]<=intervals[j][0]) ans++;
             }
-            else cnt--;
         }
         return ans;
+        // List<int[]> li = new ArrayList<>();
+        // for(int inte[]:intervals){
+        //     li.add(new int[]{inte[0],1});
+        //     li.add(new int[]{inte[1],-1});
+        // }
+        // Collections.sort(li,(a,b)->{
+        //     if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
+        //     return Integer.compare(b[1],a[1]);
+        // });
+        // int ans=0;
+        // int cnt=0;
+        // for(int val[]:li){
+        //     if(val[1]==1){
+        //         ans+=cnt;
+        //         cnt++;
+        //     }
+        //     else cnt--;
+        // }
+        // return ans;
     }
 }
