4ddda5e1794507ad8c2415b20eaba24bbae77744
Mekala Sujan <120329192+sujanmekala64@users.noreply.github.com>
2026-09-26 11:35:14 +0530

    Time: 44 ms (33.03%) | Memory: 93 MB (52.94%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,9 +1,7 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
         HashMap<String,String> map = new HashMap<>();
-        for(List<String> li:knowledge){
-            map.put(li.get(0),li.get(1));
-        }
+        for(List<String> li:knowledge) map.put(li.get(0),li.get(1));
         StringBuilder sb=new StringBuilder();
         StringBuilder ans=new StringBuilder();
         int open=0;
@@ -12,7 +10,7 @@ class Solution {
             else if(p==')'){
                 if(!map.containsKey(sb.toString())) ans.append("?");
                 else ans.append(map.get(sb.toString()));
-                sb=new StringBuilder();
+                sb.setLength(0);
                 open=0;
             }
             else if(open==1) sb.append(p);
